b94f6146c6872aa8a45efe979a33e0a2fe1f2754
Kaue Miziara <kauemiziara@gmail.com>
2025-03-10 08:41:43 -0300

    feat: implementa classes de modelo, serviço e repository para a entidade Usuário

---
+++ src/main/java/com/fiap/ecb/api_marcacao_consultas/model/Usuario.java
@@ -0,0 +1,22 @@
+package com.fiap.ecb.api_marcacao_consultas.model;
+
+import jakarta.persistence.*;
+import lombok.Data;
+
+@Data
+@Entity
+@Table(name = "usuarios")
+public class Usuario {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String nome;
+    private String email;
+    private String senha;
+
+    /**
+     * Tipo de usuário. <p>
+     * Pode ser Paciente, Médico ou Admin
+     */
+    private String tipo;
+}
